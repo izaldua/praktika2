5170803992b1a3767ba77fac874f2550f919a619
izaldua <ikerzalduni@gmail.com>
2025-04-01 19:20:15 +0000

    praktikaren bigarren zatia lortua

--- programa.c
+++ programa.c
@@ -2,15 +2,15 @@
 #include <string.h>
 
 void function(char *input) {
-	char buffer[64];
-	strcpy(buffer, input);
+        char buffer[64];
+        strcpy(buffer, input);
 }
 
 int main(int argc, char *argv[]) {
-	if (argc > 1) {
-		function(argv[1]);
-	} else {
-		printf("Usage: %s <input>\n", argv[0]);
-	}
-	return(0);
+        if (argc > 1) {
+                function(argv[1]);
+        } else {
+                printf("Usage: %s <input>\n", argv[0]);
+        }
+        return(0);
 }
